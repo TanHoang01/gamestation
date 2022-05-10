3b12864725334b2a68b3ccc1777e34e92c766097
TanHoang01 <manhtan12327@gmail.com>
2022-05-11 04:08:50 +0700

    profile page update

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -16,6 +16,13 @@ class Profile extends StatefulWidget {
 }
 
 class _Profile extends State<Profile> {
+  final nameHolder = TextEditingController();
+  final addressHolder = TextEditingController();
+ 
+  clearTextInput(){
+    nameHolder.clear();
+    addressHolder.clear();
+  }
   bool tappedYes = false;
   userModel userInfor = userModel(
       id: '',
@@ -55,7 +62,7 @@ class _Profile extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.only(left: 16, top: 25),
+      padding: EdgeInsets.only(left: 16, top: 25, right: 16),
       child: ListView(
         children: [
           Text("Profile Information",
@@ -85,73 +92,130 @@ class _Profile extends State<Profile> {
               )
             ],
           )),
-          buildTextField("Full Name", "Tan Hoang"),
+          buildTextFieldName("Full Name", "Tan Hoang"),
           buildTextField("Email", "Tan@gmail.com"),
           buildTextField("Phone", "0123456789"),
-          buildTextField("Address", "Ho Chi Minh"),
-          buildTextField("Purchased items", "1"),
-          buildTextField("Sold items", "1"),
+          buildTextFieldAddress("Address", "Ho Chi Minh"),
           SizedBox(height: 20),
           Column(
             children: [
-              Container(
-                width: 150,
-                height: 40,
-                child: DecoratedBox(
-                  decoration: ShapeDecoration(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      color: primaryColor),
-                  child: Theme(
-                    data: Theme.of(context).copyWith(
-                        buttonTheme: ButtonTheme.of(context).copyWith(
-                            materialTapTargetSize:
-                                MaterialTapTargetSize.shrinkWrap)),
-                    child: OutlineButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      child: Text('Change Password'),
-                      onPressed: () => {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ChangePassword(),
-                            ))
-                      },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 40,
+                    child: DecoratedBox(
+                      decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          color: primaryColor),
+                      child: Theme(
+                        data: Theme.of(context).copyWith(
+                            buttonTheme: ButtonTheme.of(context).copyWith(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap)),
+                        child: OutlineButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          child: Text('Save'),
+                          onPressed: () => {
+                            
+                          },
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              SizedBox(height: 10),
-              Container(
-                width: 150,
-                height: 40,
-                child: DecoratedBox(
-                  decoration: ShapeDecoration(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      color: primaryColor),
-                  child: Theme(
-                    data: Theme.of(context).copyWith(
-                        buttonTheme: ButtonTheme.of(context).copyWith(
-                            materialTapTargetSize:
-                                MaterialTapTargetSize.shrinkWrap)),
-                    child: OutlineButton(
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30))),
-                        child: Text('Log out'),
-                        onPressed: () async {
-                          final action = await AlertDialogs.yesCancelDialog(
-                              context, 'Logout', 'Are you sure to quit?');
-                          if (action == DialogsAction.yes) {
-                            setState(() => tappedYes = true);
-                          }
-                        }),
+                  SizedBox(height: 10),
+                  Container(
+                    width: 150,
+                    height: 40,
+                    child: DecoratedBox(
+                      decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          color: Colors.blue),
+                      child: Theme(
+                        data: Theme.of(context).copyWith(
+                            buttonTheme: ButtonTheme.of(context).copyWith(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap)),
+                        child: OutlineButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          child: Text('Cancel'),
+                          onPressed: () => {
+                            clearTextInput()
+                          },
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                ]
+              ),     
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                        width: 150,
+                        height: 40,
+                        child: DecoratedBox(
+                          decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30))),
+                              color: Colors.green),
+                          child: Theme(
+                            data: Theme.of(context).copyWith(
+                                buttonTheme: ButtonTheme.of(context).copyWith(
+                                    materialTapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap)),
+                            child: OutlineButton(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(30))),
+                              child: Text('Change Password'),
+                              onPressed: () => {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ChangePassword(),
+                                    ))
+                              },
+                            ),
+                          ),
+                        ),
+                      ),      
+                  Container(
+                    width: 150,
+                    height: 40,
+                    child: DecoratedBox(
+                      decoration: ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          color: Colors.red),
+                      child: Theme(
+                        data: Theme.of(context).copyWith(
+                            buttonTheme: ButtonTheme.of(context).copyWith(
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.shrinkWrap)),
+                        child: OutlineButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30))),
+                            child: Text('Log out'),
+                            onPressed: () async {
+                              final action = await AlertDialogs.yesCancelDialog(
+                                  context, 'Logout', 'Are you sure to quit?');
+                              if (action == DialogsAction.yes) {
+                                setState(() => tappedYes = true);
+                              }
+                            }),
+                      ),
+                    ),
+                  ),
+                ]
               ),
-              SizedBox(height: 10),
+              SizedBox(height: 5),
               Container(
                 child: FlatButton(
                   child: Text('About us'),
@@ -176,9 +240,44 @@ class _Profile extends State<Profile> {
               fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black)),
     );
   }
+
+  TextField buildTextFieldName(String labletext, String placeholder) {
+    return TextField(
+      controller: nameHolder,
+      focusNode: new AlwaysFocusNode(),
+      decoration: InputDecoration(
+          suffixIcon: Icon(Icons.edit),
+          labelText: labletext,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black)),
+    );
+  }
+
+   TextField buildTextFieldAddress(String labletext, String placeholder) {
+    return TextField(
+      controller: addressHolder,
+      focusNode: new AlwaysFocusNode(),
+      decoration: InputDecoration(
+          suffixIcon: Icon(Icons.edit),
+          labelText: labletext,
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: placeholder,
+          hintStyle: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black)),
+    );
+  }
 }
 
 class AlwaysDisabledFocusNode extends FocusNode {
   @override
   bool get hasFocus => false;
 }
+
+class AlwaysFocusNode extends FocusNode {
+  @override
+  bool get hasFocus => true;
+}
+ 
+  
